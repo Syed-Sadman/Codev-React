312bde98ce6f7cd5ae70cf0b68b4cdf18de9c650
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-05-19 16:56:07 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-function App() {
-  return (
-    <div>hello world</div>
-  );
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+//import ReactDOM from 'react-dom';
+import './index.js'
+
+
+class App extends Component {
+  render() {
+    return(
+    <div className="App">
+      <Greet name="Viswas"/>
+    </div>
+    );
+    }  
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import ReactDOM from 'react-dom';
 //import reportWebVitals from './reportWebVitals';
 
 
-class Greet extends React.Component{
-    render(){
-    return(<h1>
-      Hello people
-    </h1>);
-    }
+// class Greet extends React.Component{
+//     render(){
+//     return(<h1>
+//       Hello people
+//     </h1>);
+//     }
+// }
+
+const Greet=props=>{
+  console.log(props);
+  return(
+    <div>
+      <button>action</button>
+      <h1>Hello {props.name}</h1>
+    
+    </div>
+    
+    );
 }
 
 ReactDOM.render(<Greet></Greet>,document.getElementById('root'));
+
+
+export default Greet;
