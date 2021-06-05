aa3e0b9c73401ee51925699ddd35ef43ee6b2307
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-06-06 03:30:22 +0600

    React.memo HOC

--- src/comp/ParentComp.js
+++ src/comp/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react'
-import PureComp from './PureComp';
-import RegComp from './RegComp';
+import MemoComp from './MemoComp';
+// import PureComp from './PureComp';
+// import RegComp from './RegComp';
 
 class ParentComp extends Component {
     constructor(props) {
@@ -23,9 +24,10 @@ class ParentComp extends Component {
         console.log('***************Parent comp rendered****************')
         return (
             <div>
-               Parent Component 
-               <RegComp name={this.state.name} />
-               <PureComp name={this.state.name}   />
+               Parent Component
+               <MemoComp name={this.state.name}/> 
+               {/* <RegComp name={this.state.name} />
+               <PureComp name={this.state.name}   /> */}
             </div>
         )
     }
