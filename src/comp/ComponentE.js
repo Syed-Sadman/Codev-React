7aa00fc429eef13f72140b411b072cafb2d7a831
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-06-24 16:40:14 +0600

    context 3

--- src/comp/ComponentE.js
+++ src/comp/ComponentE.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 class ComponentE extends Component {
+    static contextType=UserContext;         //// ComponentE.contextType=UserContext; it can be written instead this line
     render() {
         return (
             <div>
+               This is a new context  {this.context}
                 <ComponentF/>         
             </div>
         )
     }
 }
+
+// ComponentE.contextType=UserContext;
 
 export default ComponentE
--- src/comp/userContext.js
+++ src/comp/userContext.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-const UserContext= React.createContext()
+const UserContext= React.createContext("sad evolution")
 const UserProvider=UserContext.Provider
 const UserConsumer=UserContext.Consumer
 
 export {UserProvider,UserConsumer}
+
+export default UserContext;
